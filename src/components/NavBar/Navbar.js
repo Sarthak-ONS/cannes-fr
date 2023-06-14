ae0b68650558c3eb490c2936abee127d3c0f12f7
Sarthak Agarwal <57105611+Sarthak-ONS@users.noreply.github.com>
2023-06-14 18:07:18 +0530

    Hidden Ui for Login

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -3,10 +3,18 @@ import "./Navbar.css";
 
 import logo from "../../Assets/logo.png";
 
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { BsCart, BsPerson } from "react-icons/bs";
 
 const Navbar = () => {
+  const location = useLocation();
+
+  const pathName = location.pathname;
+
+  console.log(pathName);
+
+  const isAuthPage = pathName.includes("auth");
+
   return (
     <div className="app__navbar">
       <div className="app__navbar-logo">
@@ -15,14 +23,18 @@ const Navbar = () => {
         </NavLink>
       </div>
       <div className="app__navbar-links">
-        <NavLink className="app__navbar-link" to={"/cart"}>
-          <BsCart className="icon" size={20} />
-          <p className="app__navbar-link-text">Cart</p>
-        </NavLink>
-        <NavLink className="app__navbar-link" to={"/auth/login"}>
-          <BsPerson className="icon" size={20} />
-          <p className="app__navbar-link-text">Login</p>
-        </NavLink>
+        {!isAuthPage && (
+          <NavLink className="app__navbar-link" to={"/cart"}>
+            <BsCart className="icon" size={20} />
+            <p className="app__navbar-link-text">Cart</p>
+          </NavLink>
+        )}
+        {!isAuthPage && (
+          <NavLink className="app__navbar-link" to={"/auth/login"}>
+            <BsPerson className="icon" size={20} />
+            <p className="app__navbar-link-text">Login</p>
+          </NavLink>
+        )}
       </div>
     </div>
   );
--- src/pages/AuthPages/LoginPage.js
+++ src/pages/AuthPages/LoginPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./LoginPage.css";
 import { Form, NavLink } from "react-router-dom";
 
-import { AiOutlineGoogle } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
 
 const LoginPage = () => {
   return (
@@ -11,65 +11,28 @@ const LoginPage = () => {
         Style Elevated,<br></br> Shop with Confidence!
       </div>
       <div className="login__form">
-        <form class="form">
-          <p class="form-title">Sign in to your account</p>
-          <p class="signup-link">
+        <Form className="form">
+          <p className="form-title">Sign in to your account</p>
+          <p className="signup-link">
             New Here?
             <NavLink to="/auth/signup"> Create a account</NavLink>
           </p>
-          <div class="input-container">
-            <input placeholder="Enter email" type="email" />
-            <span>
-              <svg
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                  stroke-width="2"
-                  stroke-linejoin="round"
-                  stroke-linecap="round"
-                ></path>
-              </svg>
-            </span>
+          <div className="input-container">
+            <input placeholder="Enter email" required type="email" />
           </div>
-          <div class="input-container">
-            <input placeholder="Enter password" type="password" />
-
-            <span>
-              <svg
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  stroke-width="2"
-                  stroke-linejoin="round"
-                  stroke-linecap="round"
-                ></path>
-                <path
-                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                  stroke-width="2"
-                  stroke-linejoin="round"
-                  stroke-linecap="round"
-                ></path>
-              </svg>
-            </span>
+          <div className="input-container">
+            <input placeholder="Enter password" required type="password" />
           </div>
-          <button class="submit" type="submit">
+          <button className="submit" type="submit">
             Sign in
           </button>
 
           <hr />
-          <button class="submit social" type="submit">
-            <AiOutlineGoogle size={25} className="google-icons"/>
+          <button className="submit social" type="submit">
+            <FcGoogle size={25} className="google-icons" />
             Sign in with Google
           </button>
-        </form>
+        </Form>
       </div>
     </div>
   );
