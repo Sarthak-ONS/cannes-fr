95da658cfae6014ac8f1bc7b66a84914ac0a0d89
Sarthak Agarwal <57105611+Sarthak-ONS@users.noreply.github.com>
2023-06-14 17:30:08 +0530

    Added Login form UI

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Navbar.css";
 
+import logo from "../../Assets/logo.png";
+
 import { NavLink } from "react-router-dom";
 import { BsCart, BsPerson } from "react-icons/bs";
 
@@ -8,7 +10,9 @@ const Navbar = () => {
   return (
     <div className="app__navbar">
       <div className="app__navbar-logo">
-        <NavLink to={"/"}>Cannes</NavLink>
+        <NavLink to={"/"}>
+          <img src={logo}></img>
+        </NavLink>
       </div>
       <div className="app__navbar-links">
         <NavLink className="app__navbar-link" to={"/cart"}>
@@ -17,7 +21,7 @@ const Navbar = () => {
         </NavLink>
         <NavLink className="app__navbar-link" to={"/auth/login"}>
           <BsPerson className="icon" size={20} />
-          <p className="app__navbar-link-text">Signup</p>
+          <p className="app__navbar-link-text">Login</p>
         </NavLink>
       </div>
     </div>
