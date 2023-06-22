d1b7abc17df3697b759ebd77fc9d4c1083b69a19
Sarthak Agarwal <57105611+Sarthak-ONS@users.noreply.github.com>
2023-06-22 18:23:04 +0530

    Added Ratings Feature too

--- src/pages/ProductDescriptionPage.js
+++ src/pages/ProductDescriptionPage.js
@@ -1,15 +1,19 @@
 import React, { useContext, useState, useRef } from "react";
-import { useLoaderData } from "react-router-dom";
+import { redirect, useLoaderData, useNavigate } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
 
+import { AiFillStar } from "react-icons/ai";
+
 import Modal from "../components/Modals/Modal";
+import RatingInput from "../components/Ratings/Rating";
 
 import "./ProductDescriptionPage.css";
 import CartContext from "../store/cart-context";
+import AuthContext from "../store/auth-context";
 
 import { getAuthToken } from "../utils/isAuth";
 
@@ -17,9 +21,14 @@ const ProductDescriptionPage = () => {
   const data = useLoaderData();
   const reviewtextRef = useRef();
   const cartCtx = useContext(CartContext);
+  const authCtx = useContext(AuthContext);
   const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
 
   const [showModal, setshowModal] = useState(false);
+
+  const [showReviewModal, setshowReviewModal] = useState(false);
+  const [rating, setRating] = useState();
   const [isReviewErrorText, setIsReviewErrorText] = useState(false);
   const [reviewError, setReviewError] = useState("");
 
@@ -54,7 +63,7 @@ const ProductDescriptionPage = () => {
           "Content-Type": "application/json",
           Authorization: "Bearer " + getAuthToken(),
         },
-        body: JSON.stringify({ text: reviewtextRef.current.value }),
+        body: JSON.stringify({ text: reviewtextRef.current.value, rating }),
       }
     );
 
@@ -66,6 +75,10 @@ const ProductDescriptionPage = () => {
       setIsReviewErrorText(true);
       setReview(jObj.message);
     }
+  };
+
+  const ratingHandler = (rating) => {
+    setRating(rating);
   };
 
   return (
@@ -85,28 +98,42 @@ const ProductDescriptionPage = () => {
           <h2 className="ProductDescriptionPage-content__title">
             {data && data.product && data.product.name}
           </h2>
+          <h4 style={{ display: "flex", alignItems: "center" }}>
+            <p>
+              {data.product.ratings && (
+                <>{calculateRatings(data.product.ratings)}</>
+              )}
+            </p>
+            <AiFillStar color="yellow" />
+          </h4>
           <h3>Rs. {data.product.price}</h3>
           <h4>Brand: {data && data.product && data.product.brand}</h4>
           <h5>{data && data.product && data.product.description}</h5>
           <p>
             <button
               onClick={() => {
-                cartCtx.qtyChangeHandler(data.product._id, true);
-                setOpen(true);
+                if (authCtx.email) {
+                  cartCtx.qtyChangeHandler(data.product._id, true);
+                  setOpen(true);
+                  return;
+                }
+                navigate("/auth/login");
               }}
               className="add-to-cart-cta"
               type="button"
             >
               Add to Cart
             </button>
-            <button
-              className="add-to-cart-cta"
-              onClick={() => {
-                setshowModal(true);
-              }}
-            >
-              Write a review
-            </button>
+            {authCtx.email && (
+              <button
+                className="add-to-cart-cta"
+                onClick={() => {
+                  setshowModal(true);
+                }}
+              >
+                Write a review
+              </button>
+            )}
           </p>
         </div>
       </div>
@@ -130,6 +157,8 @@ const ProductDescriptionPage = () => {
                 ref={reviewtextRef}
                 placeholder="Write review in 150 words"
               />
+              <br />
+              <RatingInput submitRating={ratingHandler} />
               <button type="submit">Publish</button>
             </form>
           </Modal>
@@ -171,6 +200,18 @@ const ProductDescriptionPage = () => {
       </Snackbar>
     </>
   );
+};
+
+const calculateRatings = (ratings) => {
+  console.log(ratings);
+  let totalRatings = 0;
+  ratings.forEach((item) => {
+    totalRatings = totalRatings + item.rating;
+  });
+  console.log("RATING IS CALCULATED", totalRatings / ratings.length);
+  return (totalRatings / ratings.length).toString() === "NaN"
+    ? 0
+    : totalRatings / ratings.length;
 };
 
 export async function loader({ request, params }) {
